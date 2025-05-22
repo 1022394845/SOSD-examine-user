bceb8795c9330366a59b5d5030357340863318be
1022394845 <1022394845@qq.com>
2025-05-22 20:01:39 +0800

    全局注册v-icon
    全局注册v-icon
    
    全局注册v-icon

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import router from './router'
 // 默认样式
 import '@/styles/common.scss'
 
+// iconfont字体图标
+import VIcon from '@/components/v-icon/index.vue'
+
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.component('v-icon', VIcon)
 
 app.mount('#app')
