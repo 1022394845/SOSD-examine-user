f8d563b322fe29ec913fd96b4323616467f42807
1022394845 <1022394845@qq.com>
2025-05-22 19:58:46 +0800

    component:v-icon

---
+++ src/components/v-icon/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({ name: String })
+</script>
+
+<template>
+  <i class="v-icon" :class="'v-icon-' + props.name"></i>
+</template>
+
+<style lang="scss">
+@import url('./iconfont.css');
+</style>
